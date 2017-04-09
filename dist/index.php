7ba31eb8261e31b5339b6ceac5510d2b6fc32d9a
Loïc Hamet <loic.hamet@gmail.com>
2017-04-09 12:21:46 +0200

    initial commit

---
+++ dist/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="<?php echo $site->language()->code ?>" class="no-js">
+<head>
+	<meta charset="utf-8">
+	<title>Loïc Hamet _ Web development</title>
+
+	<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+	<link rel="stylesheet" href="/assets/css/app.css" />
+</head>
+
+<body class="layout">
+	<div id="container">
+		<div class="loader"></div>
+		<div class="content">
+
+		</div>
+	</div>
+	<script type="text/javascript" src="/assets/js/app.js"></script>
+</body>
