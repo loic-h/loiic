de661275b949fed651657ec31d2ad43be4922988
Loïc Hamet <loic.hamet@gmail.com>
2017-07-04 17:33:17 +0200

    add shorts to ls

--- src/js/cmds/list.js
+++ src/js/cmds/list.js
@@ -1,14 +1,15 @@
-import {keywords} from '.';
+import {keywords, shorts} from '.';
 
 export default {
 	key: 'list',
 	alias: ['ls'],
 	help: 'list all available commands',
 	out(ctx) {
+		const words = Object.keys(keywords).concat(Object.keys(shorts));
 		return `
 <div class="list">
 	<ul class="cmd-list">
-		${Object.keys(keywords).sort().map(k => `
+		${words.sort().map(k => `
 			<li class="cmd-list__item">
 				<a href="${k}" class="cmd">${k}</a>
 			</li>
