7cb05a620acdce9a694d8df8f8b751019edad5b3
Loïc Hamet <loic.hamet@gmail.com>
2017-07-06 23:10:52 +0200

    favicons

--- dist/index.php
+++ dist/index.php
@@ -4,6 +4,15 @@
 	<meta charset="utf-8">
 	<title>Loïc Hamet _ Web development</title>
 
+	<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicons/favicon-16x16.png">
+	<link rel="manifest" href="/assets/favicons/manifest.json">
+	<link rel="mask-icon" href="/assets/favicons/safari-pinned-tab.svg" color="#66ffc1">
+	<link rel="shortcut icon" href="/assets/favicons/favicon.ico">
+	<meta name="msapplication-config" content="/assets/favicons/browserconfig.xml">
+	<meta name="theme-color" content="#343c3f">
+
 	<meta name="viewport" content="width=device-width,initial-scale=1.0" />
 	<link rel="stylesheet" href="/assets/css/app.css" />
 </head>
